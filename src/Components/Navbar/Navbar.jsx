dca4a2e8703614c579adafdcbd8071c50ede22c5
Subir Das <dasjoy104@gamil.com>
2024-04-06 00:09:55 +0600

    home edited

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,22 +3,44 @@ import logo from '../../assets/Logo/Calendly-logo.png'
 
 const Navbar = () => {
   return (
-    <div className='flex'>
     <div>
-        <img src={logo} alt="" />
-    </div>
-    <div>
-        <ul className='flex'>
-            <li>individual</li>
-            <li>Home</li>
-            <li>team</li>
-        </ul>
-    </div>
-    <div>
-        <button>login</button>
-        <button>signup</button>
-    </div>
-    </div>
+        <header className="p-4 dark:bg-gray-100 dark:text-gray-800">
+	<div className="container flex justify-between h-16 mx-auto">
+		<a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
+			<img src={logo} alt="" />
+		</a>
+		<ul className="items-stretch hidden space-x-3 lg:flex">
+			<li className="flex">
+				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Individuals</a>
+			</li>
+			<li className="flex">
+				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Teams</a>
+			</li>
+			<li className="flex">
+				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Enterprise</a>
+			</li>
+			<li className="flex">
+				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Products</a>
+			</li>
+			<li className="flex">
+				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Pricing</a>
+			</li>
+			<li className="flex">
+				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Resources</a>
+			</li>
+		</ul>
+		<div className="items-center flex-shrink-0 hidden lg:flex">
+			<button className="self-center px-8 py-3 rounded">Login</button>
+			<button className="self-center px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Get Started</button>
+		</div>
+		<button className="p-4 lg:hidden">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-800">
+				<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+			</svg>
+		</button>
+	</div>
+</header>
+   </div>
   )
 }
 
