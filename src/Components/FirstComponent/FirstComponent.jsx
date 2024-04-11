e20646ac1d458dbca55df4f5190c36733740bdfa
Subir Das <dasjoy104@gamil.com>
2024-04-11 19:38:44 +0600

    1st component added

---
+++ src/Components/FirstComponent/FirstComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FirstComponent = () => {
+  return (
+    <div>
+      <p>SOLUTION</p>
+      <p>Calendly for the work you do</p>
+      <p>Across a range of uses, Calendly helps people and teams build efficiency, professionalism, and results into the work they do.</p>
+      <button>Contact sales</button>
+    </div>
+  )
+}
+
+export default FirstComponent
