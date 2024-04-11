57f304430c7c450fb7835490c01a5cf4ba9600a8
Subir Das <dasjoy104@gamil.com>
2024-04-11 20:24:29 +0600

    second-component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import FirstComponent from '../FirstComponent/FirstComponent'
+import SecondComponent from '../SecondComponent/SecondComponent'
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
       <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
     </div>
   )
 }
---
+++ src/Components/SecondComponent/SecondComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SecondComponent = () => {
+  return (
+    <div className='text-center my-20'>
+      <p className='text-sm text-[#006BFE] font-semibold'>SOLUTION</p>
+      <p className='text-4xl my-5 font-bold'> Calendly can solve your specfic pain points</p>
+      
+    </div>
+  )
+}
+
+export default SecondComponent
